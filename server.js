35cf63fa104a4787a9bce47c5255199adfae8d5a
Jason Yu <ycm.jason@gmail.com>
2017-04-24 12:10:38 +0800

    parse both multipart and formdata. also print the content-type for debugging

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
 var multer = require('multer');
 
 var PORT =  process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use(multer().none());
 
-app.post('*', multer().none(), function(req, res) {
+app.post('*', function(req, res) {
   console.log('POST ' + req.originalUrl);
+  console.log(req.get('Content-Type'));
   console.log(req.body);
   console.log();
   res.end();
