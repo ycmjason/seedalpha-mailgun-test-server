b3e0a6d82edef5f8371069630557333cf226bfdb
Jason Yu <ycm.jason@gmail.com>
2017-04-24 11:58:29 +0800

    changed body-parser to multer to accept multipart/form-data

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var app = require('express')();
-var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var PORT =  process.env.PORT || 3000;
 
-app.use(bodyParser.json());
 
-app.post('*', function(req, res) {
+app.post('*', multer().none(), function(req, res) {
   console.log('POST ' + req.originalUrl);
   console.log(req.body);
   console.log();
