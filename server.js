f11e6b86783c70a1ffc0ebe122c4764721442b43
Jason Yu <ycm.jason@gmail.com>
2017-04-24 11:28:53 +0800

    initial server listening to all paths on POST

---
+++ server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+var PORT =  process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.post('*', function(req, res) {
+  console.log('POST ' + req.originalUrl);
+  console.log(req.body);
+  console.log();
+  res.end();
+});
+
+app.listen(PORT);
